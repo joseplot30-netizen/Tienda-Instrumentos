889b889028b064fa2f22a66f562fa5c6654314e1
Jose Guzman <joseplot30@gmail.com>
2026-03-17 19:06:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,23 @@ from functools import wraps
 import mysql.connector
 import os
 
+
 sentry_sdk.init(
     dsn="https://864c5f4eb2dffc66fd0e90f04f987352@o4511062458564608.ingest.us.sentry.io/4511062484123648",
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
+    # Enable sending logs to Sentry
+    enable_logs=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # Set profile_session_sample_rate to 1.0 to profile 100%
+    # of profile sessions.
+    profile_session_sample_rate=1.0,
+    # Set profile_lifecycle to "trace" to automatically
+    # run the profiler on when there is an active transaction
+    profile_lifecycle="trace",
 )
 
 app = Flask(__name__)
